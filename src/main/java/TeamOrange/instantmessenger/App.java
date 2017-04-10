ac7d3bcb9e9738904ab50a1c73ac9429357f6ed5
Tim Bell <Tim95Bell@gmail.com>
2017-04-10 22:40:45 +1000

    Created and setup Babbler branch
    Created Babbler branch and set it up using eclipse and the eclipse maven
    plugin (m2e)

---
+++ src/main/java/TeamOrange/instantmessenger/App.java
@@ -0,0 +1,14 @@
+package TeamOrange.instantmessenger;
+
+
+import rocks.xmpp.core.session.XmppClient;
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+    	XmppClient xmppClient;
+        System.out.println( "Hello World!" );
+    }
+}
