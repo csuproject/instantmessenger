862cb364bb5215eeefc6ae5f9d2d432d6c960fc3
Tim Bell <Tim95Bell@gmail.com>
2017-04-10 23:05:49 +1000

    Created and setup Smack branch
    Created Smack branch and set it up using eclipse and the eclipse maven
    plugin (m2e)

---
+++ src/main/java/TeamOrange/instantmessenger/App.java
@@ -0,0 +1,14 @@
+package TeamOrange.instantmessenger;
+
+
+import org.jivesoftware.smack.XMPPException;
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+    	XMPPException e;
+        System.out.println( "Hello World!" );
+    }
+}
