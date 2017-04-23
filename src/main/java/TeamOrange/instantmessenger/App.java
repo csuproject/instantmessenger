79edd7eb1353e971f458c677b1402238a053591a
Tim Bell <Tim95Bell@gmail.com>
2017-04-24 00:26:49 +1000

    Started basic JavaFX GUI

--- src/main/java/TeamOrange/instantmessenger/App.java
+++ src/main/java/TeamOrange/instantmessenger/App.java
@@ -1,22 +1,29 @@
 package TeamOrange.instantmessenger;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class App
+public class App extends Application
 {
-	Client client;
+	private Client client;
+	private Stage stage;
 
-    public static void main( String[] args )
-    {
-    	App app = new App();
-    	app.run();
-    }
+//    public static void main( String[] args )
+//    {
+//    	App app = new App();
+//    	app.run();
+//    }
 
-    public void run(){
-    	init();
-
-    	CLI cli = new CLI(this);
-    	cli.run();
-    }
+//    public void run(){
+//    	init();
+//
+////    	CLI cli = new CLI(this);
+////    	cli.run();
+//    	AccountScreen accountScreen = new AccountScreen(this);
+//    	accountScreen.launch(null);
+//    	//Application.launch(AccountScreen.class);
+//    }
 
     public void init(){
     	client = new BabblerClient("teamorange.space", () -> messageListener(), () -> presenceListener(), () -> rosterListener());
@@ -43,5 +50,24 @@ public class App
     	return client;
     }
 
+    public boolean login(String username, String password){
+    	return client.login(username, password);
+    }
+
+    public void createAccount(String username, String password){
+    	client.createUser(username, password);
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		init();
+
+		this.stage = primaryStage;
+		stage.setTitle("Confide");
+
+		Scene scene = new Scene(new AccountScreen(this), 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
 
 }
