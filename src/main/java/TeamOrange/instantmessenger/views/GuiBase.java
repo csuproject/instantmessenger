6134625c4f4028dce679f35680abca47441e7c11
Tim Bell <Tim95Bell@gmail.com>
2017-04-27 20:41:43 +1000

    Added packages: models, views, controllers. Added GuiBase.java and AccountScreen.java

---
+++ src/main/java/TeamOrange/instantmessenger/views/GuiBase.java
@@ -0,0 +1,28 @@
+package TeamOrange.instantmessenger.views;
+
+import TeamOrange.instantmessenger.App;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GuiBase extends Application {
+	private Stage stage;
+	private App app;
+
+	public static void main(String[] args){
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		app = new App();
+		app.init();
+
+		this.stage = primaryStage;
+		stage.setTitle("Confide");
+
+		Scene scene = new Scene(new AccountScreen(app), 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
