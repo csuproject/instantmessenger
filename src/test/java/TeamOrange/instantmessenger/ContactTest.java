9cad5f637863a27bdaa432d8c35566723293c6b7
Tim Bell <Tim95Bell@gmail.com>
2017-07-23 19:47:56 +1000

    Implemented a test to test contact management funcitonality

---
+++ src/test/java/TeamOrange/instantmessenger/ContactTest.java
@@ -0,0 +1,87 @@
+package TeamOrange.instantmessenger;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import TeamOrange.instantmessenger.models.AppMessage;
+import TeamOrange.instantmessenger.models.AppPresence;
+import TeamOrange.instantmessenger.xmpp.BabblerBase;
+import exceptions.ConfideXmppException;
+
+public class ContactTest {
+
+	public static void main(String[] args){
+		BabblerBase client = init();
+		Scanner in = new Scanner(System.in);
+		String name = "";
+		String password = "";
+		boolean running = true;
+
+		try {
+			System.out.print("name: ");
+			name = in.nextLine();
+			System.out.print("password: ");
+			password = in.nextLine();
+			client.login(name, password);
+			while(running){
+				System.out.print("(add {name} | delete {name} | block {name} | print | exit): ");
+				String input = in.nextLine();
+				String[] splitInput = input.split(" ");
+				if(input.toUpperCase().equals("EXIT")){
+					running = false;
+				} else if(input.toUpperCase().equals("PRINT")){
+					printContacts(client);
+				} else if(splitInput.length == 2) {
+					if(splitInput[0].toUpperCase().equals("ADD")){
+						client.addContact(splitInput[1]);
+					} else if(splitInput[0].toUpperCase().equals("DELETE")){
+						client.removeContact(splitInput[1]);
+					} else if(splitInput[0].toUpperCase().equals("BLOCK")){
+						client.blockUser(splitInput[1]);
+						client.printBlockList();
+					}
+				}
+			}
+		} catch (ConfideXmppException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void printContacts(BabblerBase client){
+		LinkedList<String> contacts = client.getContacts();
+		System.out.println("----- Contacts -----");
+		for(String s : contacts)
+			System.out.println(s);
+		System.out.println("--------------------");
+	}
+
+	public static BabblerBase init(){
+		BabblerBase client =
+				new BabblerBase("teamorange.space",
+						appMessage->messageListener(appMessage),
+						appPresence->presenceListener(appPresence),
+						() -> rosterListener());
+
+		client.setupConnection();
+    	try {
+			client.connect();
+		} catch (ConfideXmppException e) {
+			System.out.println("------- exception conecting ---------");
+			e.printStackTrace();
+		}
+    	return client;
+	}
+
+	private static void rosterListener() {
+		// TODO Auto-generated method stub
+	}
+
+	private static void presenceListener(AppPresence appPresence) {
+		// TODO Auto-generated method stub
+	}
+
+	private static void messageListener(AppMessage appMessage) {
+		// TODO Auto-generated method stub
+	}
+
+}
